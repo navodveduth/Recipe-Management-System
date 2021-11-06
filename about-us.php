8e39e2feb7ee617c0ad49ee8ac19419538883bad
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:26:46 +0530

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>About US</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+
+    <link rel="stylesheet" href="styles/info.css">
+
+    <!-- Icons -->
+    <script src="https://kit.fontawesome.com/0ba0f621bd.js" crossorigin="anonymous"></script>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Ubuntu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Glory:wght@300&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <?php require "header.php" ?>
+
+    <div class="info-section">
+        <div class="topic">
+            <label>About Us</label>
+            <br><br>
+            <hr>
+        </div>
+        <div class="info-container">
+            <ul>
+                <label id="li-lb"> Who are we ?</label>
+                <li id="li-style">We are a group of students at SLIIT who are passionate about web development</li>
+                <br>
+                <label id="li-lb"> Our team members ?</label>
+                <li id="li-style">IT20159030 Lekamge L.R.S.T</li>
+                <li id="li-style">IT21110948 Weerawarna N.V</li>
+                <li id="li-style">IT21062988 Munasinghe K.D.V.S</li>
+                <li id="li-style">IT21111938 Dilusha N.H.A.S</li>
+                <li id="li-style">IT21001598 Medawaththa K.S</li>
+            </ul>
+        </div>
+    </div>
+    <?php require "footer.php" ;?>
+
+</body>
+
+</html>
