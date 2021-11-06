d420f33fda794faabfb7dad4f19a74a6cb3f5eef
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:27:13 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+    <title>Footer</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="styles/footer.css" />
+    <script src="https://kit.fontawesome.com/0ba0f621bd.js" crossorigin="anonymous"></script>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="footerLeft">
+            <a href="http://localhost:8080/assignment/src/home.php"><img id="logo" src="images/logo.png" alt="logo.png" /></a>
+        </div>
+        <div class="footerMiddle">
+            <fieldset class="socialMedia">
+                    <legend>Follow us on social media</legend>
+                    <a href="https://www.facebook.com">
+                        <i class="fab fa-facebook fa-3x" style="color:#FFFFFF"></i>  
+                    </a>
+                    <a href="https:/www.instagram.com">
+                        <i class="fab fa-instagram fa-3x" style="color:#FFFFFF"></i>
+                    </a>
+                    <a href="https:/www.pinterest.com">
+                        <i class="fab fa-pinterest fa-3x" style="color:#FFFFFF"></i>
+                    </a>
+            </fieldset>
+            <h6 id="copyright">Copyright by MLB_01.02_09. All rights reserved</h6>
+        </div>
+        <div class="footerRight">
+            <div class="footerMenu">
+                <ul>
+                    <li><a href="http://localhost:8080/assignment/src/about-us.php">About us</a></li>
+                    <li><a href="http://localhost:8080/assignment/src/contact-us.php">Contact us</a></li>
+                    <li><a href="http://localhost:8080/assignment/src/FAQ.php">FAQ</a></li>
+                    <li><a href="http://localhost:8080/assignment/src/terms.php">Terms and conditions</a></li>
+                    <li><a href="http://localhost:8080/assignment/src/policy.php">Privacy policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
