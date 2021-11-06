60f3e03cd79e91973b3db972a2ac0e1a7233c6c2
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:27:39 +0530

    Add files via upload

---
+++ terms.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Terms and conditions</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/info.css">
+</head>
+
+<body>
+    <?php require "header.php" ;?>
+    <div class="info-section">
+        <div class="topic">
+            <label>Terms and conditions</label>
+            <br><br>
+            <hr>
+        </div>
+        <div class="info-container">
+            <ul>
+                <label id="li-lb">Copyright</label>
+                <li id="li-style"> All recipes and pictures are a property of Recipe Hunt and you cannot copy or post
+                    them
+                    anywhere without the permission of the proper authourity.</li>
+                <br>
+                <label id="li-lb"> Accounts and posts</label>
+                <li id="li-style">Recipe Hunt has a right to delete user accounts and change user permissions without
+                    prior notice. If any account or post vioaltes our privacy policy their accounts will be permanantely
+                    deleted.</li>
+            </ul>
+        </div>
+    </div>
+    <?php require "footer.php" ;?>
+</body>
+
+</html>
