51ab98d199ac6de3fe90d5a66b69a67e7a239469
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:27:06 +0530

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>FAQ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="styles/info.css">
+</head>
+
+<body>
+    <?php require "header.php" ;?>
+    <div class="info-section">
+        <div class="topic">
+            <label>Frequently Asked Questions</label>
+            <br><br>
+            <hr>
+        </div>
+        <div class="info-container">
+            <ul>
+                <label id="li-lb">What is this website about?</label>
+                <li id="li-style">This website is about food recipes. We write recipes read your comments and reply to
+                    them.
+                </li><br>
+                <label id="li-lb">Do you write paid posts?</label>
+                <li id="li-style">No we are not. Recipe Hunt is a free website.</li><br>
+                <label id="li-lb">Do we allow guest posts?</label>
+                <li id="li-style">We do not accept guest posts yet. I you want to post something you have to be a
+                    registered
+                    user first.
+                </li><br>
+            </ul>
+        </div>
+    </div>
+
+    <?php require "footer.php" ;?>
+</body>
+
+</html>
