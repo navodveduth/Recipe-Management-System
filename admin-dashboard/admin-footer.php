66b8fd9f11f9a6fa9bc58100dbf63af9d5d2688d
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:29:06 +0530

    Add files via upload

---
+++ admin-dashboard/admin-footer.php
@@ -0,0 +1,32 @@
+<html>
+
+    <head>
+        <title>Admin Footer</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="../styles/theme.css" />
+        <style>
+            body {
+                margin: 0;
+            }
+            .footer {
+            text-align: center;
+            color: white;
+            background-color: #141414;
+            width: 100%;
+            height: 3rem;
+            padding: 1rem;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <div class = footer>
+            Developed by MLB_01.02_09
+        </div>
+        
+
+    </body>
+
+</html>
