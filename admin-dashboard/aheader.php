42c61a2ef8ef7bf6d8f7cbee545cac455b9f8f18
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:29:23 +0530

    Add files via upload

---
+++ admin-dashboard/aheader.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <title>Header</title>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="../styles/header.css" />
+    <script src="https://kit.fontawesome.com/0ba0f621bd.js" crossorigin="anonymous"></script>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+<body>
+    <div class="container">
+        <div class="headerLeft">
+            <img id="logo" src="../images/logo.png" alt="logo.png" />
+        </div>
+        <div class="headerRight">
+            <div>
+                <a id="login" href="http://localhost:8080/assignment/src/admin-dashboard/Adminlogin.php">
+                    <i class="fas fa-user"></i> Login
+                </a>
+                <br />
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
