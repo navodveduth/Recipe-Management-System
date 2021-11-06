6fbbf9cdb4ab327a767ec2a2fddac298b36d8d17
Navod Veduth <54998118+navodveduth@users.noreply.github.com>
2021-11-06 19:27:33 +0530

    Add files via upload

---
+++ policy.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Privacy policy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/info.css">
+
+</head>
+
+<body>
+    <?php require "header.php" ;?>
+
+    <div class="info-section">
+        <div class="topic">
+            <label>Privacy policy</label>
+            <br><br>
+            <hr>
+        </div>
+        <div class="info-container">
+            <ul>
+                <label id="li-lb">Information we collect</label>
+                <li id="li-style">We collect data like your email, location, your age and what you searched in our
+                    website</li>
+                <br>
+            </ul>
+        </div>
+    </div>
+
+    <?php require "footer.php" ;?>
+
+</body>
+
+</html>
